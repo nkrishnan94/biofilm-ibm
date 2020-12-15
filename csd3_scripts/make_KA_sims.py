7de7f6d6152b5ff59ff586bad0f11194219da547
nkrishnan94 <npk13@case.edu>
2020-12-15 08:31:33 -0500

    added scripts

--- csd3_scripts/make_KA_sims.py
+++ csd3_scripts/make_KA_sims.py
@@ -6,14 +6,15 @@ from datetime import datetime
 
 path = os.getcwd()
 
-z_length = np.array([6,12,18])
+z_length = np.array([3,6,9])
 #x_length = np.array([100,150,200])
 #deform_scale = np.array([.5,.7,.9])
                         
 time_str = datetime.now()
 d=time_str.strftime("%m_%d_%y_%H_%M_%S")
 new_path = path+"/sim_files_KA"
-samples= 1
+filmlngth =100
+samples = 100
 try:
     os.mkdir(new_path)
 except:
@@ -26,7 +27,7 @@ for z in z_length:
 
         time_str = datetime.now()
         d_1=time_str.strftime("%m_%d_%y_%H_%M_%S")
-        file_str=str(d_1)+"_thick_"+str(z)+"_len_"+str(200)+"_scale_"+str(.6)+'_rep_'+str(n)
+        file_str=str(d_1)+"_thick_"+str(z)+"_len_"+str(filmlngth)+"_scale_"+str(.6)+'_rep_'+str(n)
         f = open(new_path+"/biofilm_KA_"+str(cnt)+".inp", "a")
         f.write('#-------------------------- Initial Setup -------------------------------------#\n')
         f.write('log ' +new_path+'/log_KA_'+str(cnt)+'_'+file_str+'.log\n')
@@ -37,10 +38,10 @@ for z in z_length:
         f.write('timestep    0.01\n')
         f.write('#------------------------------------------------------------------------------#\n')
         f.write('#-------------------------- Atomic Setup --------------------------------------#\n')
-        f.write('region film block 0 200 0 '+str(z)+' 0 6 \n')# Geometrical region.
+        f.write('region film block 0 '+ str(filmlngth) +' 0 '+str(z)+' 0 6 \n')# Geometrical region.
         f.write('create_box 2 film \n')# Create simulation box.
-        f.write('create_atoms 1 random ' +str(int(6*200*.75*z)) +' '+str(n+1)+' '+'film\n') # Create atoms at random within film.
-        f.write('create_atoms 2 random ' +str(int(6*200*.75*z)) +' '+str(10000+n+1)+' '+'film\n') # Create atoms at random within film.
+        f.write('create_atoms 1 random ' +str(int(6*filmlngth*.75*z)) +' '+str(n+1)+' '+'film\n') # Create atoms at random within film.
+        f.write('create_atoms 2 random ' +str(int(6*filmlngth*.75*z)) +' '+str(10000+n+1)+' '+'film\n') # Create atoms at random within film.
         f.write('neigh_modify every 1 delay 0 check yes\n')
 
         f.write('mass 1 1\n')
@@ -52,14 +53,17 @@ for z in z_length:
 
         f.write('minimize 1.0e-4 1.0e-6 1000 1000\n')
         f.write('#-------------------------------- Outputs -------------------------------------#\n')
-        f.write('thermo 1000\n')
-        f.write('thermo_style custom step temp press density ly\n')
+
         f.write('compute stress all centroid/stress/atom NULL\n')
         f.write('compute press all pressure NULL pair\n')
+        f.write('compute area all voronoi/atom surface all\n')
+        f.write('compute tot_area all reduce sum c_area[3]\n')
         f.write('dump 1 all atom 500 ' +new_path+'/traj_KA_'+file_str+'.out\n')
-        f.write('dump 3 all atom 500 ' +new_path+'/traj_KA_'+file_str+'.xyz\n')
+        f.write('dump 3 all xyz 500 ' +new_path+'/traj_KA_'+file_str+'.xyz\n')
         f.write('dump_modify 1 sort id append yes\n')
         f.write('dump 2 all custom 500 ' +new_path+'/stress_KA_'+file_str+'.out c_stress[*]\n')
+        f.write('thermo 500\n')
+        f.write('thermo_style custom step temp press density ly c_tot_area\n')
 
         f.write('#------------------------------------------------------------------------------#\n')
         f.write('#------------------------- Running the Simulation -----------------------------#\n')
